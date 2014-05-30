a026180c3c65e817f47aff9e39c5a1589dea74ce
Grant Gavares <gavares@gmail.com>
2014-05-30 15:46:58 -0700

    More work on artifact name

--- src/main/scala/com/playi/SbtPlayI.scala
+++ src/main/scala/com/playi/SbtPlayI.scala
@@ -56,8 +56,8 @@ object SbtPlayI extends Plugin {
   ) ++ s3Settings ++ Seq(
     S3.progress in S3.upload := true,
     mappings in S3.upload := {
-      val fName = assembly.value.getName
-      Seq((new java.io.File(s"target/$fName"), s"${organization.value}/${name.value}/${version.value}/$jarName"))
+      val fName = jarName.value
+      Seq((new java.io.File(s"target/$fName"), s"${organization.value}/${name.value}/${version.value}/$fName"))
     },
     S3.host in S3.upload := s3Repo,
     credentials += {
